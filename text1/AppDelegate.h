9e39e68ab5b1fa2d938793df6822f361abf47506
傻子露 <shazilu@shaziludemacbook-pro.local>
2016-06-24 02:12:24 +0800

    Initial Commit

---
+++ text1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  text1
+//
+//  Created by 傻子露 on 16/6/24.
+//  Copyright © 2016年 Liqingquan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ text1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  text1
+//
+//  Created by 傻子露 on 16/6/24.
+//  Copyright © 2016年 Liqingquan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
